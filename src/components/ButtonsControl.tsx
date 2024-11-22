3637e45550bdb9a73ce832860ce6bb13ac3958b1
juan-diego-v <zxcvwert3458@gmail.com>
2024-11-22 16:10:18 +0900

    feat: impl-buttons-control-comp

--- src/components/ButtonsControl.tsx
+++ src/components/ButtonsControl.tsx
@@ -1,14 +1,20 @@
-import { Button, Flex } from "@chakra-ui/react"
+import { Button, Flex } from "@chakra-ui/react";
+import { FC } from "react";
 
-export default function () {
+const ButtonsControl: FC<{
+    onStart: () => void;
+    onPick: () => void;
+}> = ({ onStart, onPick }) => {
     return (
         <Flex position='absolute' zIndex={10} right='64px' bottom='64px' gap='16px'>
-            <Button w='96px' h='96px' colorScheme="red" rounded='full'>
+            <Button w='96px' h='96px' colorScheme="red" rounded='full' onClick={onStart}>
                 Start
             </Button>
-            <Button w='96px' h='96px' colorScheme="blue" rounded='full'>
+            <Button w='96px' h='96px' colorScheme="blue" rounded='full' onClick={onPick}>
                 Pick
             </Button>
         </Flex>
     )
 }
+
+export default ButtonsControl;
