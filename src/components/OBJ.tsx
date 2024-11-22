a518cb915de4ed05f08f39f94b6638fac7840682
juan-diego-v <zxcvwert3458@gmail.com>
2024-11-22 16:10:54 +0900

    feat: update-obj-comp

--- src/components/OBJ.tsx
+++ src/components/OBJ.tsx
@@ -1,12 +1,16 @@
-import { FC } from 'react';
+import { forwardRef, ForwardRefRenderFunction } from 'react';
 import { Group } from 'three';
 import { InteractiveObject3DEventMap } from 'three/examples/jsm/Addons.js';
 
-const OBJ: FC<{
-    obj: Group<InteractiveObject3DEventMap>,
-    position?: any;
-}> = ({ obj, position }) => {
-    return <primitive object={obj} position={position} />
+const OBJ: ForwardRefRenderFunction<
+    any,
+    {
+        obj: Group<InteractiveObject3DEventMap>,
+        position?: any;
+        rotation?: any;
+    }
+> = ({ obj, position, rotation }, ref) => {
+    return <primitive ref={ref} object={obj} position={position} rotation={rotation} />
 }
 
-export default OBJ;
+export default forwardRef(OBJ);
