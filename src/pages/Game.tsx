70987098a34afbd676f3aa1be54b5da9d53ee59c
juan-diego-v <zxcvwert3458@gmail.com>
2024-11-22 16:11:43 +0900

    feat: update-game

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,11 +1,31 @@
+import { Box, Modal, ModalContent, ModalOverlay } from '@chakra-ui/react';
+import { Canvas } from '@react-three/fiber';
+import { useRef, useState } from 'react';
+import ButtonsControl from '../components/ButtonsControl';
+import JoystickControl from '../components/JoystickControl';
+import ProgressBar from '../components/ProgressBar';
 import Scene from '../components/Scene';
-import GameProvider from '../providers/GameProvider';
 
 const Game = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [progress, setProgress] = useState(0);
+
+    const ref = useRef<any>();
+
     return (
-        <GameProvider>
-            <Scene />
-        </GameProvider>
+        <Box position='relative' w='100vw' h='100vh'>
+            <Modal isOpen={isLoading} onClose={() => { }}>
+                <ModalOverlay bg='linear-gradient(135deg, #6f00ff, #00ffcc)' />
+                <ModalContent my={0} py='120px' h='full' display='flex' justifyContent='end' alignItems='center' bg='none' shadow='none'>
+                    <ProgressBar progress={progress} />
+                </ModalContent>
+            </Modal>
+            <JoystickControl onJoystick={(x, z) => ref.current?.onJoystick(x, z)} />
+            <ButtonsControl onStart={() => { }} onPick={() => ref.current?.onPick()} />
+            <Canvas shadows='soft'>
+                <Scene ref={ref} setIsLoading={setIsLoading} setProgress={setProgress} />
+            </Canvas>
+        </Box>
     );
 };
 
