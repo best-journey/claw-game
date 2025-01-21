f6592d6ea9a3427b65e9fc188811fdff97b347ee
juan-diego-v <zxcvwert3458@gmail.com>
2025-01-22 00:16:32 +0900

    chore: move-provider-to-main

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import { ChakraProvider } from '@chakra-ui/react'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Game from './pages/Game'
 
 function App() {
   return (
-    <ChakraProvider>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Game />} />
-        </Routes>
-      </Router>
-    </ChakraProvider>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Game />} />
+      </Routes>
+    </Router>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
 
-import './index.css'
+import './index.css';
+import Provider from './providers/index.tsx';
 
-createRoot(document.getElementById('root')!).render(<App />)
+createRoot(document.getElementById('root')!).render(
+    <Provider>
+        <App />
+    </Provider>
+);
