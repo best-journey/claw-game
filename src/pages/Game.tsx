1a86d75d62193b1a3cb5c81ab9997bd9beb173e5
best-journey <144888274+best-journey@users.noreply.github.com>
2025-03-04 13:42:13 +0900

    chore: remove-loading-text

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -18,9 +18,6 @@ const Game = () => {
                 <ModalOverlay bg='linear-gradient(135deg, #6f00ff, #00ffcc)' />
                 <ModalContent my={0} py='120px' h='full' display='flex' justifyContent='end' alignItems='center' bg='none' shadow='none'>
                     <ProgressBar progress={progress} />
-                    <Text fontSize='xx-large' color='#dd3c6e'>
-                        Loading...
-                    </Text>
                 </ModalContent>
             </Modal>
             <JoystickControl onJoystick={(x, z) => ref.current?.onJoystick(x, z)} />
